731c16436ff82991594cb48a48a81d98f770f6d3
wanglei <wanglei@freetek.cc>
2022-10-28 17:51:23 +0800

    新建爬虫项目

---
+++ lei-project/lei-spider/src/main/java/lei/spider/chandao/ChandaoApplication.java
@@ -0,0 +1,13 @@
+package lei.spider.chandao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChandaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChandaoApplication.class, args);
+    }
+
+}
---
+++ lei-project/lei-spider/src/test/java/lei/spider/chandao/ChandaoApplicationTests.java
@@ -0,0 +1,13 @@
+package lei.spider.chandao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChandaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
