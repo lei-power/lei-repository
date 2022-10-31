f925d06be928e2b31a7f7f93f070816f9c2cf39f
wanglei <wanglei@freetek.cc>
2022-11-01 00:37:55 +0800

    修改

--- lei-project/lei-spider/chandao/src/main/java/lei/spider/processor/ChandaoIndexProcessor.java
+++ lei-project/lei-spider/chandao/src/main/java/lei/spider/processor/ChandaoIndexProcessor.java
@@ -1,6 +1,7 @@
 package lei.spider.processor;
 
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -12,7 +13,7 @@ import us.codecraft.webmagic.selector.Selectable;
  *@Date 2022/10/28 17:55
  ***/
 
-public class ChandaoProcessor implements PageProcessor {
+public class ChandaoIndexProcessor implements PageProcessor {
 
     private Site site = Site.me().setRetryTimes(3).setSleepTime(100).addCookie("Cookie", "lang=zh-cn; device=desktop; keepLogin=on; za=wanglei; preProductID=1; theme=default; preBranch=24; goback=%7B%22qa%22%3A%22http%3A%5C%2F%5C%2Fchandao.freetek.cc%5C%2Fzentao%5C%2Fbug-browse-4-0-unclosed-0-openedBy_asc-43-20.html%22%2C%22doc%22%3A%22http%3A%5C%2F%5C%2Fchandao.freetek.cc%5C%2Fzentao%5C%2Fdoc-objectLibs-custom-0-37-389.html%22%2C%22execution%22%3A%22http%3A%5C%2F%5C%2Fchandao.freetek.cc%5C%2Fzentao%5C%2Fdoc%5C%2F%22%2C%22project%22%3A%22http%3A%5C%2F%5C%2Fchandao.freetek.cc%5C%2Fzentao%5C%2Fdoc-objectLibs-custom-0-37-219.html%22%7D; tab=doc; zentaosid=dkamm4j36e8hceifhgfj35e1a5; zp=b12271bd1d2115234d57ccb725310690eb4c0731; windowWidth=1920; windowHeight=977").setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36");
 
@@ -21,29 +22,37 @@ public class ChandaoProcessor implements PageProcessor {
     public void process(Page page) {
         Selectable custom = page.getHtml();
         //目录
-        String title = custom.xpath("//button[@id='currentBranch']/text()").get();
+        String title = StringUtils.trim("src/main/resources/" + custom.xpath("//button[@id='currentBranch']/text()").get());
         //目录
-        List<String> titleList = custom.xpath("//div[@class='tree-group']/span").all();
-        //正文
-        String content = custom.xpath("//div[@class='table-row']").get();
-        //附件
-        List<String> fujainList = custom.xpath("//div[@class='detail-content']/ul").all();
-        page.putField("titleList", titleList);
-        page.putField("content", content);
-        page.putField("fujainList", fujainList);
-        //创建目录
-        String path=title;
-        for (String target : titleList) {
-            if (target.contains("href")) {
-
-            }
+        List<Selectable> nodes = custom.xpath("//div[@class='detail']/ul/li").nodes();
+        if (nodes != null && nodes.size() > 0) {
+            genNodes(nodes, title);
         }
 
+        // page.putField("titleList", titleList);
 
         //        PrintWriter printWriter = new PrintWriter(new FileWriter(getFile(filename)));
         //        printWriter.write(ToStringBuilder.reflectionToString(o));
         //        printWriter.close();
 
+    }
+
+    private void genNodes(List<Selectable> nodes, String title) {
+        String path = title;
+        for (Selectable node : nodes) {
+            List<Selectable> subNodes = node.xpath("/ul/li").nodes();
+            if (subNodes == null || subNodes.size() == 0) {
+                List<Selectable> docNodes = nodes.get(0).xpath("li[@class='doc']").nodes();
+                if (docNodes != null && docNodes.size() > 0) {
+                    for (Selectable docNode : docNodes) {
+                        Selectable xpath = docNode.xpath("/a/text()");
+                        System.err.println(xpath.toString());
+                    }
+                }
+            }
+            genNodes(subNodes, path);
+
+        }
     }
 
     @Override
@@ -52,12 +61,7 @@ public class ChandaoProcessor implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        Spider.create(new ChandaoProcessor())
-                .addUrl("http://chandao.freetek.cc/zentao/doc-tablecontents-custom-0-37.html")
-                .addUrl("http://chandao.freetek.cc/zentao/doc-tablecontents-custom-0-28.html")
-                .addUrl("http://chandao.freetek.cc/zentao/doc-tablecontents-custom-0-53.html")
-                .addUrl("http://chandao.freetek.cc/zentao/doc-tablecontents-custom-0-124.html")
-                .thread(5).run();
+        Spider.create(new ChandaoIndexProcessor()).addUrl("http://chandao.freetek.cc/zentao/doc-tablecontents-custom-0-37.html").addUrl("http://chandao.freetek.cc/zentao/doc-tablecontents-custom-0-28.html").addUrl("http://chandao.freetek.cc/zentao/doc-tablecontents-custom-0-53.html").addUrl("http://chandao.freetek.cc/zentao/doc-tablecontents-custom-0-124.html").thread(10).run();
     }
 
 }
