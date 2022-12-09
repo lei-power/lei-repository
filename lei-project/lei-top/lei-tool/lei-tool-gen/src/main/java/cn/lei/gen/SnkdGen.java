52943c336f82020a71a34b28b74ef5fc5cadbc7f
wanglei <wanglei@freetek.cc>
2022-12-09 18:08:03 +0800

    【修改】变更代码样式

--- lei-project/lei-top/lei-tool/lei-tool-gen/src/main/java/cn/lei/gen/SnkdGen.java
+++ lei-project/lei-top/lei-tool/lei-tool-gen/src/main/java/cn/lei/gen/SnkdGen.java
@@ -1,6 +1,5 @@
 package cn.lei.gen;
 
-
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.file.FileReader;
 import cn.hutool.db.DbUtil;
@@ -60,7 +59,7 @@ public class SnkdGen {
             System.out.println("columnDefinitionList:" + columnDefinitionList);
             //生成代码
             ConfigContext configContext = new ConfigContext();
-            configContext.setOutputPath(this.getClass().getResource("/").getPath()+"/snkd_gen" + DateUtil.formatTime(new Date())+"/");
+            configContext.setOutputPath(StringUtils.substring(this.getClass().getResource("/").getPath(), 0, this.getClass().getResource("/").getPath().length() - 8) + "/snkd" + DateUtil.formatDate(new Date()) + "/");
             configContext.setSourcePath(cn.hutool.core.io.FileUtil.getAbsolutePath("snkd_vm"));
             configContext.setTargetName(fileName);
             configContext.setTargetTable(tableName);
@@ -100,81 +99,42 @@ public class SnkdGen {
                         String.format("%s.java", configContext.getTargetName()),    //文件名
                         VelocityUtil.render("entity.vm", context));                 //模板生成内容
 
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "manage/", String.format("%sManage.java", configContext.getTargetName()), VelocityUtil.render("manage.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "manage/", String.format("%sManage.java", configContext.getTargetName()), VelocityUtil.render("manage.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "mapper/", String.format("%sMapper.java", configContext.getTargetName()), VelocityUtil.render("mapper.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "mapper/", String.format("%sMapper.xml", configContext.getTargetName()), VelocityUtil.render("xml.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/vo/" + name.toLowerCase(), String.format("Gov%sVO.java", configContext.getTargetName()), VelocityUtil.render("vo_gov.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "transfer/", String.format("Trans.java", configContext.getTargetName()), VelocityUtil.render("transfer.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/controller/", String.format("Gov%sController.java", configContext.getTargetName()), VelocityUtil.render("Govcontroller.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/service/", String.format("Gov%sService.java", configContext.getTargetName()), VelocityUtil.render("Govservice.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/" + name.toLowerCase(), String.format("Gov%sListReq.java", configContext.getTargetName()), VelocityUtil.render("req_list.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/" + name.toLowerCase(), String.format("Gov%sGetReq.java", configContext.getTargetName()), VelocityUtil.render("req_get.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/" + name.toLowerCase(), String.format("Gov%sAddReq.java", configContext.getTargetName()), VelocityUtil.render("req_add.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/" + name.toLowerCase(), String.format("Gov%sEditReq.java", configContext.getTargetName()), VelocityUtil.render("req_edit.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/" + name.toLowerCase(), String.format("Gov%sDelReq.java", configContext.getTargetName()), VelocityUtil.render("req_del.vm", context));
 
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "mapper/", String.format("%sMapper.java", configContext.getTargetName()), VelocityUtil.render("mapper.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "mapper/", String.format("%sMapper.xml", configContext.getTargetName()), VelocityUtil.render("xml.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/vo/"
-                        + name.toLowerCase(), String.format("Gov%sVO.java", configContext.getTargetName()), VelocityUtil.render("vo_gov.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "transfer/", String.format("Trans.java", configContext.getTargetName()), VelocityUtil.render("transfer.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "biz/gov/controller/", String.format("Gov%sController.java", configContext.getTargetName()), VelocityUtil.render("Govcontroller.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "biz/gov/service/", String.format("Gov%sService.java", configContext.getTargetName()), VelocityUtil.render("Govservice.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/"
-                        + name.toLowerCase(), String.format("Gov%sListReq.java", configContext.getTargetName()), VelocityUtil.render("req_list.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/"
-                        + name.toLowerCase(), String.format("Gov%sGetReq.java", configContext.getTargetName()), VelocityUtil.render("req_get.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/"
-                        + name.toLowerCase(), String.format("Gov%sAddReq.java", configContext.getTargetName()), VelocityUtil.render("req_add.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/"
-                        + name.toLowerCase(), String.format("Gov%sEditReq.java", configContext.getTargetName()), VelocityUtil.render("req_edit.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/gov/model/req/"
-                        + name.toLowerCase(), String.format("Gov%sDelReq.java", configContext.getTargetName()), VelocityUtil.render("req_del.vm", context));
                 //创建admin相关文件
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "admin/", String.format("Admin%sController.java", configContext.getTargetName()), VelocityUtil.render("admincontroller.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "admin/", String.format("Admin%sService.java", configContext.getTargetName()), VelocityUtil.render("adminservice.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "admin/", String.format("Admin%sListReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_list.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "admin/", String.format("Admin%sGetReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_get.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "admin/", String.format("Admin%sAddReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_add.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "admin/", String.format("Admin%sEditReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_edit.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "admin/", String.format("Admin%sDelReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_del.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "admin/", String.format("Admin%sController.java", configContext.getTargetName()), VelocityUtil.render("admincontroller.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "admin/", String.format("Admin%sService.java", configContext.getTargetName()), VelocityUtil.render("adminservice.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "admin/", String.format("Admin%sListReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_list.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "admin/", String.format("Admin%sGetReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_get.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "admin/", String.format("Admin%sAddReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_add.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "admin/", String.format("Admin%sEditReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_edit.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "admin/", String.format("Admin%sDelReq.java", configContext.getTargetName()), VelocityUtil.render("adminreq_del.vm", context));
 
                 //co
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/vo/"
-                        + name.toLowerCase(), String.format("Co%sVO.java", configContext.getTargetName()), VelocityUtil.render("vo_co.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "transfer/", String.format("Trans.java", configContext.getTargetName()), VelocityUtil.render("transfer.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/vo/" + name.toLowerCase(), String.format("Co%sVO.java", configContext.getTargetName()), VelocityUtil.render("vo_co.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "transfer/", String.format("Trans.java", configContext.getTargetName()), VelocityUtil.render("transfer.vm", context));
 
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "biz/co/controller/", String.format("Co%sController.java", configContext.getTargetName()), VelocityUtil.render("CoController.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath()
-                        + "biz/co/service/", String.format("Co%sService.java", configContext.getTargetName()), VelocityUtil.render("COService.vm", context));
-
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/"
-                        + name.toLowerCase(), String.format("Co%sListReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_list.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/"
-                        + name.toLowerCase(), String.format("Co%sGetReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_get.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/"
-                        + name.toLowerCase(), String.format("Co%sAddReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_add.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/"
-                        + name.toLowerCase(), String.format("Co%sEditReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_edit.vm", context));
-                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/"
-                        + name.toLowerCase(), String.format("Co%sDelReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_del.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/controller/", String.format("Co%sController.java", configContext.getTargetName()), VelocityUtil.render("CoController.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/service/", String.format("Co%sService.java", configContext.getTargetName()), VelocityUtil.render("COService.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/" + name.toLowerCase(), String.format("Co%sListReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_list.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/" + name.toLowerCase(), String.format("Co%sGetReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_get.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/" + name.toLowerCase(), String.format("Co%sAddReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_add.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/" + name.toLowerCase(), String.format("Co%sEditReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_edit.vm", context));
+                FileUtil.writeFile(configContext.getOutputPath() + "biz/co/model/req/" + name.toLowerCase(), String.format("Co%sDelReq.java", configContext.getTargetName()), VelocityUtil.render("co_req_del.vm", context));
             }
         });
     }
-
 
     public static void main(String[] args) {
         new SnkdGen().snkdGenFlow();
